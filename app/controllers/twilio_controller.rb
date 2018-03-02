96f2f76f6f8b424821fa6ab4e841cc7babcff141
lnoellaho <lnoellaho@gmail.com>
2018-03-02 14:16:07 -0800

    deploy

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -10,4 +10,16 @@ class TwilioController < ApplicationController
     end
     render :xml => response.to_xml
   end
+
+  def conference
+    response = Twilio::TwiML::VoiceResponse.new
+    response.gather(input: 'dtmf', timeout: 10, numDigits: 4) do |gather|
+      gather.say('Please enter your four digit pin.')
+    end
+    response.dial do |dial|
+      dial.conference('Room 1234')
+    end
+    render :xml => response.to_xml
+  end
+
 end
