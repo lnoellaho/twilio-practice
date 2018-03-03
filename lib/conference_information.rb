1aac2c4a0b476dee3e1645a07a0eb2add11b1bd8
lnoellaho <lnoellaho@gmail.com>
2018-03-02 16:04:56 -0800

    working dial

--- lib/conference_information.rb
+++ lib/conference_information.rb
@@ -3,15 +3,15 @@ module Conference_information
   require 'twilio-ruby'
 
   # Get your Account SID and Auth Token from twilio.com/console
-  account_sid = 'ACb74c519d16b0a17c2c0cf8aaa2dd41b1'
+  account_sid = 'Sid'
   auth_token = 'your_auth_token'
 
   # Initialize Twilio Client
   @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-  @conference = @client.api.conferences('CFbbe46ff1274e283f7e3ac1df0072ab39')
+  @conference = @client.api.conferences('conference id')
                        .fetch
 
   puts @conference.status
-  
+
 end
